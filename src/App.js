d8075cbb3f69910c5c125427e976df3b4fbad74d
Catherine Laserna <49669196+cjlaserna@users.noreply.github.com>
2024-01-25 17:11:51 -0800

    nsWASD :wq :wq :x :x

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Profile from "./pages/Profile";
+import Experiences from "./pages/Experiences";
+import Achievements from "./pages/Achievements";
+import Contact from "./pages/Contact";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+	return (
+		<Router>
+			<>
+				<div className="holy-grail-grid">
+					{/* <HeartRain /> */}
+
+					<Routes>
+						<Route path="/" element={<Profile />} />
+						<Route path="/experiences" element={<Experiences />} />
+						<Route path="/achievements" element={<Achievements />} />
+						<Route path="/contact" element={<Contact />} />
+					</Routes>
+				</div>
+			</>
+		</Router>
+	);
+};
 
 export default App;
+
